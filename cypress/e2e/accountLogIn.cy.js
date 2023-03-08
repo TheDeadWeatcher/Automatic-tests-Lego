0b8b452d0ae5fd780435399d63aaa05616930b3c
Bartosz <jasykbartosz@gmail.com>
2023-03-08 15:35:45 +0100

    '#chromeWebsecurity: false'

--- cypress/e2e/accountLogIn.cy.js
+++ cypress/e2e/accountLogIn.cy.js
@@ -9,13 +9,14 @@ describe('Should log in to Lego account', () => {
 
     // test nie przechodzi na przegladarce electron, chrome, edge, firefox ok  ?! lego  wykryło bota 
 
-    it('Should be correctly log in to Lego account ', () => {
+    it.only('Should be correctly log in to Lego account ', () => {
         cy.get('[fill-rule="nonzero"]').click();
         cy.get('[data-test="legoid-login-button"]').click();
         cy.get('[name="username"]').type("testlego123456@gmail.com");
         cy.get('[name="password"]').type("Rujupyse123!");
         cy.get('[data-testid="loginBtn"]').click();
-        cy.get("h1").should("have.text", "We�re sorry for the interruption");
+        cy.get('[fill-rule="nonzero"]').click();
+        cy.url().should("eq", "https://www.lego.com/pl-pl/my-account");
     });
 
 
@@ -25,7 +26,7 @@ describe('Should log in to Lego account', () => {
       cy.get('[name="username"]').type("testlego123456@gmail.com");
       cy.get('[name="password"]').type("Rujupyse123!11");
       cy.get('[data-testid="loginBtn"]').click();
-      cy.get('h1').should("contain", "We�re sorry");
+      cy.get(".login-error").should("contain", "hasło nie odpowiadają");
     });
     
     
