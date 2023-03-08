104ab918d63573df5431109584eaabd6361a91a6
Bartosz <jasykbartosz@gmail.com>
2023-03-08 14:23:16 +0100

    '#login panel work'

--- cypress/e2e/accountLogIn.cy.js
+++ cypress/e2e/accountLogIn.cy.js
@@ -7,7 +7,7 @@ describe('Should log in to Lego account', () => {
       cy.acceptEnterPopups(); 
     });
 
-    // test nie przechodzi na przegladarce electron ?! lego wykryło test bota 
+    // test nie przechodzi na przegladarce electron, chrome, edge, firefox ok  ?! lego  wykryło bota 
 
     it('Should be correctly log in to Lego account ', () => {
         cy.get('[fill-rule="nonzero"]').click();
@@ -17,5 +17,16 @@ describe('Should log in to Lego account', () => {
         cy.get('[data-testid="loginBtn"]').click();
         cy.get("h1").should("have.text", "We�re sorry for the interruption");
     });
+
+
+    it("Should faild log in to Lego account ", () => {
+      cy.get('[fill-rule="nonzero"]').click();
+      cy.get('[data-test="legoid-login-button"]').click();
+      cy.get('[name="username"]').type("testlego123456@gmail.com");
+      cy.get('[name="password"]').type("Rujupyse123!11");
+      cy.get('[data-testid="loginBtn"]').click();
+      cy.get('h1').should("contain", "We�re sorry");
+    });
+    
     
 });
